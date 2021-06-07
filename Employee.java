de3336583ba48bd848677cbff8f41f37a38a0619
Thomas McDowell <46029055+trmcdowell@users.noreply.github.com>
2021-06-07 12:08:14 -0500

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,7 +1,7 @@
 /**
  * Employees are People who work
  * @author Douglas Jones
- * @author Tom McDowell
+ * @author Thomas McDowell
  * @version 11/2/2020
  * Status: broken off of MP8 solution; it works, but see BUG notices
  * @see Person
